405109dd85efa62ea5924603d85166c06a33d2a7
Karen Pointon <karen.pointon.webdev@gmail.com>
2022-04-08 12:27:26 +0200

    Ajout de la gestion likes/dislikes

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -117,5 +117,113 @@ exports.deleteSauce = (req, res, next) => {
 };
 
 exports.rateSauce = (req, res, next) => {
-  console.log(req.body);
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => {
+      let userHasLiked = sauce.usersLiked.indexOf(req.body.userId) != -1; // Si l'id de l'user est dans le tableau "a aimé"
+      let userHasDisliked = sauce.usersDisliked.indexOf(req.body.userId) != -1; // Si l'id de l'user est dans le tableau "n'a pas aimé"
+
+      if (req.body.like == 0) {
+        // Si l'user annule son like/son dislike
+
+        if (userHasLiked) {
+          // S'il avait like
+
+          Sauce.updateOne(
+            { _id: req.params.id },
+            {
+              $inc: { likes: -1 },
+              $pull: { usersLiked: req.body.userId },
+              _id: req.params.id,
+            }
+          )
+            // On retire un like pour cette sauce et l'user du tableau des users ayant aimé cette sauce
+
+            .then(() => {
+              res.status(200).json({ message: "Like retiré." });
+            }) // Confirme le retrait du like
+
+            .catch((error) => {
+              res.status(400).json({ error });
+            }); //  Signale l'erreur survenue le cas échéant
+        } else if (userHasDisliked) {
+          // S'il avait dislike
+
+          Sauce.updateOne(
+            { _id: req.params.id },
+            {
+              $inc: { dislikes: -1 },
+              $pull: { usersDisliked: req.body.userId },
+              _id: req.params.id,
+            }
+          )
+            // On retire un dislike pour cette sauce et l'user du tableau des users n'ayant pas aimé cette sauce
+
+            .then(() => {
+              res.status(200).json({ message: "Dislike retiré." });
+            })
+            .catch((error) => {
+              res.status(400).json({ error });
+            });
+        } else {
+          // Si un user essaie de retirer un like/dislike qu'il n'avait pas fait, la requête échoue avec erreur 403 ci-dessous :
+
+          return res.status(403).json({ error: "Requête non autorisée." });
+        }
+      } else if (req.body.like == 1) {
+        // Si l'user like la sauce
+
+        if (userHasLiked == false && userHasDisliked == false) {
+          // S'il n'a pas déjà like et dislike la sauce
+
+          Sauce.updateOne(
+            { _id: req.params.id },
+            {
+              $inc: { likes: 1 },
+              $push: { usersLiked: req.body.userId },
+              _id: req.params.id,
+            }
+          ) // Ajout d'un like et ajout de l'user dans le tableau des likes
+
+            .then(() => {
+              res.status(200).json({ message: "Sauce likée." });
+            })
+            .catch((error) => {
+              res.status(400).json({ error });
+            });
+        } else {
+          // Si user a déjà like et/ou dislike -> erreur 403 car techniquement impossible (sécurité prévue pour éviter la surcharge de la BDD)
+          return res.status(403).json({ error: "Requête non autorisée." });
+        }
+      } else if (req.body.like == -1) {
+        // Si l'user dislike la sauce
+
+        if (userHasDisliked == false && userHasLiked == false) {
+          // Si l'user n'a pas déjà dislike et like
+          Sauce.updateOne(
+            { _id: req.params.id },
+            {
+              $inc: { dislikes: 1 },
+              $push: { usersDisliked: req.body.userId },
+              _id: req.params.id,
+            }
+          )
+            // Ajouter un dislike pour la sauce et l'userId dans le tableau des users qui n'aiment pas la sauce
+            .then(() => {
+              res.status(200).json({ message: "Sauce dislikée." });
+            })
+            .catch((error) => {
+              res.status(400).json({ error });
+            });
+        } else {
+          // Si user a déjà like et/ou dislike -> erreur 403 car techniquement impossible (sécurité prévue pour éviter la surcharge de la BDD)
+          return res.status(403).json({ error: "Requête non autorisée." });
+        }
+      } else {
+        // Si l'user essaie de mettre autre chose que 0 (annule son choix), 1 (like) ou -1 (dislike) -> erreur 400
+        return res.status(400).json({ error: "Mauvaise requête." });
+      }
+    })
+    .catch((error) => {
+      res.status(404).json({ error });
+    }); // Si la sauce n'a pas été touvée, bretelle de la ceinture ;)
 };
